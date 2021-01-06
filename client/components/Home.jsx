ff62cec6a644de644e57c26c1a5d676d3337fa12
olivia-gambitsis <o.gambitsis@gmail.com>
2021-01-06 17:42:37 +1300

    create firebase file

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import {Link} from 'react-router-dom'
+import Learn from './Learn'
 
 
 
@@ -10,16 +11,17 @@ class Home extends React.Component {
   
   render(){
     return(
-      <div className="home-background">
+     
       <div className='home'>
         <div className='title'>
           <h1>Home</h1>  
        </div>
        <div className='start-learning'>
           <h4>CLick <Link to={'/learn'}>HERE</Link> To Start Learning!</h4>
+          <Learn/>
        </div>
       </div>
-      </div>
+      
     )
   }
 }
