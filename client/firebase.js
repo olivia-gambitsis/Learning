f6c55f58818eeda7e8686c917d96ebfe35fdbe25
olivia-gambitsis <o.gambitsis@gmail.com>
2021-01-21 16:18:34 +1300

    stopped tracking env.local

--- client/firebase.js
+++ client/firebase.js
@@ -17,3 +17,12 @@ const firebaseConfig ={
 
 export const app = firebase.initializeApp(firebaseConfig)
 export const auth = app.auth()
+
+
+
+REACT_APP_FIREBASE_API_KEY = AIzaSyCAhzPewvB8uFrGIt97A3-xuqx7Wj1fotY
+REACT_APP_FIREBASE_AUTH_DOMAIN = quiz-me-25fce.firebaseapp.com
+REACT_APP_FIREBASE_PROJECT_ID =quiz-me-25fce
+REACT_APP_FIREBASE_STORAGE_BUCKET =quiz-me-25fce.appspot.com
+REACT_APP_FIREBASE_MESSAGING_SENDER_ID =593572213930
+REACT_APP_FIREBASE_APP_ID = 1:593572213930:web:87d35fa3ad356dbe6eccd3
